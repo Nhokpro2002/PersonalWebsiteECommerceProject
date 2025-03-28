351db3dfbf506ec9475517487baa5e8f8d6d31f0
Nhokpro2002 <tuannm@newwave.com.vn>
2025-03-28 18:03:09 +0700

    Add mapper package to mapping data

--- src/main/java/com/example/laptopstorebackend/mapper/ProductConverter.java
+++ src/main/java/com/example/laptopstorebackend/mapper/ProductConverter.java
@@ -1,4 +1,17 @@
 package com.example.laptopstorebackend.mapper;
 
+import com.example.laptopstorebackend.dto.ProductDTO;
+import com.example.laptopstorebackend.entity.Product;
+
 public class ProductConverter {
+
+    public static ProductDTO covertFromEntity(Product product) {
+        return ProductDTO.builder()
+                .productName(product.getProductName())
+                .description(product.getDescription())
+                .sellingPrice(product.getSellingPrice())
+                .imageURL(product.getImageURL())
+                .build();
+    }
+
 }
